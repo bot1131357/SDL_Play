496b10b864a3059e1f94dfc0e807fb0142947c14
Yu Ang, Tan <yuang86@gmail.com>
2017-09-14 11:33:13 +1000

    Improved collision detection. Lesson learned: do not use int for zero-crossing.

--- 01_GameEngine_v2/01_GameEngine_v2.cpp
+++ 01_GameEngine_v2/01_GameEngine_v2.cpp
@@ -10,6 +10,7 @@
 #include <mutex>
 #include <atomic>
 #include <bitset>
+#include <algorithm>    // std::remove_if
 #include "SDL.h"
 #include "SDL_image.h"
 #include "01_GameEngine_v2.h"
@@ -92,14 +93,14 @@ public:
 						if (keycode != e.key.keysym.sym || t2 - t1 > chrono::milliseconds(1000 / _pollrate))
 						{
 							t1 = t2;
-							cout << "EveMan\n";
+							//cout << "EveMan\n";
 							keycode = e.key.keysym.sym;
 							_keyDownHandler(keycode);
 						}
 						break;
 					case SDL_KEYUP:
 						t1 = t2;
-						cout << "EveMan\n";
+						//cout << "EveMan\n";
 						keycode = e.key.keysym.sym;
 						_keyUpHandler(keycode);
 						break;
@@ -221,8 +222,8 @@ protected:
 	string _id;
 public:
 	SDL_Rect dstRect;
-	SDL_Rect dstRect_; // previous location
-	float x, y;
+	//SDL_Rect dstRect_; // previous location
+	float x, y, x_, y_; // better use Point2D
 	float _v_x, _v_y;
 	float _a_x, _a_y;
 	SDLEntity::SDLEntity(string id) :_id(id)
@@ -232,6 +233,15 @@ public:
 	virtual void draw(SDL_Surface* gSurface) {}
 	friend void handleCollision(SDLEntity &ref, SDLEntity &obj);
 	friend bitset<5> detectCollision(SDLEntity &ref, SDLEntity &obj);
+
+	const float get_v_x() { return _v_x; }
+	const float get_v_y() { return _v_y; }
+	// current location
+	const float get_x() { return x; }
+	const float get_y() { return y; }
+	// previous location
+	const float get_x_() { return x_; }
+	const float& get_y_() { return y_; }
 };
 class Introscreen : public SDLEntity
 {
@@ -319,14 +329,15 @@ class Ball : public SDLEntity
 {
 	SDL_Renderer* _gRenderer = NULL;
 	SDL_Texture* _texture = NULL;
+	string _imgpath;
 	//SDL_Surface* _loadedSurface;
 	SDL_Rect srcRect;
 
 public:
 	Ball(string id, SDL_Renderer* gRenderer, string imgpath) :
-		SDLEntity(id), _gRenderer(gRenderer)
+		SDLEntity(id), _gRenderer(gRenderer), _imgpath(imgpath)
 	{
-		SDL_Surface* tempSurface = IMG_Load(imgpath.c_str());
+		SDL_Surface* tempSurface = IMG_Load(_imgpath.c_str());
 		if (tempSurface == NULL)
 		{
 			throw runtime_error(string("Failed to load surface") + SDL_GetError() + "\n");
@@ -334,19 +345,20 @@ public:
 		x = uniform_dist_w(e1);
 		y = uniform_dist_h(e1);
 		//x = y = 100;
-		srcRect = { 9-1	,134-1,12,12 };
+		srcRect = { 9 - 1	,134 - 1,12,12 };
 		dstRect = { (int)x	,(int)y,12,12 };
-		dstRect_ = dstRect;
+		x_ = x;
+		y_ = y;
 
 		SDL_SetColorKey(tempSurface, SDL_TRUE, SDL_MapRGB(tempSurface->format, 0xFF, 0xFF, 0xFF));
 		//Create texture from surface pixels
 		_texture = SDL_CreateTextureFromSurface(_gRenderer, tempSurface);
 		SDL_FreeSurface(tempSurface);
-		_v_x = _v_y = 300/pollrate;
-		_a_x = _a_y = 0;
+		_v_x = _v_y = -100.0 / pollrate;
+		_a_x = _a_y = 0.0;
 	}
 	// Move
-	Ball(Ball&& other):SDLEntity(other._id)
+	Ball(Ball&& other) :SDLEntity(other._id)
 	{
 		// release the current object’s resources
 		_gRenderer = other._gRenderer;
@@ -362,9 +374,33 @@ public:
 		_a_x = other._a_x;
 		_a_y = other._a_y;
 
-		dstRect=other.dstRect;
-		srcRect=other.srcRect;
-		dstRect_ = dstRect;
+		dstRect = other.dstRect;
+		srcRect = other.srcRect;
+		x_ = other.x;
+		y_ = other.y;
+	}
+	Ball& operator=(Ball&& other)
+	{
+		// release the current object’s resources
+		this->_gRenderer = other._gRenderer;
+		this->_texture = other._texture;
+		// Make sure the destructor during move don't destroy the texture
+		other._gRenderer = NULL;
+		other._texture = NULL;
+
+		this->x = other.x;
+		this->y = other.y;
+		this->_v_x = other._v_x;
+		this->_v_y = other._v_y;
+		this->_a_x = other._a_x;
+		this->_a_y = other._a_y;
+
+		this->dstRect = other.dstRect;
+		this->srcRect = other.srcRect;
+
+		this->x_ = this->x;
+		this->y_ = this->y;
+		return *this;
 	}
 	~Ball()
 	{
@@ -377,7 +413,8 @@ public:
 	}
 	void update()
 	{
-		dstRect_ = dstRect;
+		x_ = x;
+		y_ = y;
 
 		_v_x += _a_x;
 		_v_y += _a_y;
@@ -386,16 +423,22 @@ public:
 		x += _v_x;
 		y += _v_y;
 
-		if (x < 0) if (_v_x <0)  _v_x = -_v_x;
-		if (x + dstRect.w > width) if (_v_x > 0) _v_x = -_v_x;
+		const bool BALL_BOUNCES_ON_WALL = true;
+		const int LEFT_BOUND = 0;
+		const int RIGHT_BOUND = width;
+		if (BALL_BOUNCES_ON_WALL)
+		{
+			if (x < LEFT_BOUND) if (_v_x < 0)  _v_x = -_v_x;
+			if (x + dstRect.w > RIGHT_BOUND) if (_v_x > 0) _v_x = -_v_x;
+		}
 		if (y < 0) if (_v_y <0)  _v_y = -_v_y;
 		if (y + dstRect.h > height) if (_v_y > 0) _v_y = -_v_y;
 
 		dstRect.x = x;
 		dstRect.y = y; 
 
-		const float MAX_VEL = 300/ pollrate;
-		const float MIN_VEL = 50/ pollrate;
+		const float MAX_VEL = 300.0/ pollrate;
+		const float MIN_VEL = 50.0/ pollrate;
 		// Deccelerate
 		_v_y = (_v_y < MIN_VEL && _v_y > -MIN_VEL) ? sgn(_v_y)*MIN_VEL : .999*_v_y;
 		//cout << _v_x << ", " << _v_y << endl;
@@ -404,6 +447,13 @@ public:
 	{
 		_a_x = a_x; _a_y = a_y;
 		//cout << "accel\n";
+	}
+
+	bool isOutOfScreen()
+	{
+		if (x + dstRect.w < 0) return true;
+		if (x > width) return true;
+		return false;
 	}
 };
 
@@ -428,7 +478,8 @@ public:
 		//_v_y = 2;
 		srcRect = { 10 - 1, 8 - 1,11,51 };
 		dstRect = { (int)x	,(int)y,11,51 };
-		dstRect_ = dstRect;
+		x_ = x;
+		y_ = y;
 
 		SDL_SetColorKey(tempSurface, SDL_TRUE, SDL_MapRGB(tempSurface->format, 0xFF, 0xFF, 0xFF));
 		//Create texture from surface pixels
@@ -449,7 +500,8 @@ public:
 	const float MIN_VEL = 1 / pollrate;
 	void update()
 	{
-		dstRect_ = dstRect;
+		x_ = x;
+		y_ = y;
 
 		// Deccelerate
 		_v_x = (_v_x < -MIN_VEL || _v_x > MIN_VEL) ? .9*_v_x : 0;
@@ -477,10 +529,6 @@ public:
 		_a_x = a_x; _a_y = a_y;
 		//cout << "accel\n";
 	}
-	float get_v_x() { return _v_x; }
-	float get_v_y() { return _v_y; }
-	float get_x() { return x; }
-	float get_y() { return y; }
 };
 
 
@@ -490,7 +538,6 @@ public:
 	AIPaddle(string id, SDL_Renderer* gRenderer, string imgpath) :
 		Paddle(id, gRenderer, imgpath)
 	{
-
 		x = width - 50;
 		y = 50;
 		dstRect = { (int)x	,(int)y,11,51 };
@@ -499,7 +546,8 @@ public:
 	const int ACCEL = 60;
 	void update(vector<Ball> &balls)
 	{
-		dstRect_ = dstRect;
+		x_ = x;
+		y_ = y;
 		accel(0, 0);
 		Ball* nearest_ball = nullptr;
 		int ball_max_x = 0;
@@ -521,9 +569,9 @@ public:
 			static float e1 = 0;
 			static float d_e = 0;
 			static float i_e = 0;
-			static float P = 5;
-			static float P_I = .05;
-			static float P_D = 0;
+			static float P = 2;
+			static float P_I = .0;
+			static float P_D = .5;
 
 			e2 = b.y + (b.dstRect.h/2) - y - (dstRect.h / 2);
 			d_e = e2 - e1;
@@ -695,33 +743,46 @@ public:
 	// leftmost bit indicates collision
 	bitset<5> res = 0;
 
-	if (ref._v_x>obj._v_x) 
-		if (ref.dstRect.x + ref.dstRect.w > obj.dstRect.x)  // ref right bound is more than obj left bound
-			if (ref.dstRect_.x + ref.dstRect_.w < obj.dstRect.x)  // ref right bound is previously less than obj left bound 
-				if (ref.dstRect.y + ref.dstRect.h > obj.dstRect.y)  // ref lower bound is lower than obj upper bound
-					if (ref.dstRect.y < obj.dstRect.y + obj.dstRect.h) // ref upper bound is higher than obj lower bound
+	// DEBUG CODE WHEN BALL HITS PADDLE
+	//if (ref.get_x() + ref.dstRect.w > 350)
+	//{
+	//	cout << "Action!\n";
+	//}
+	//if (ref.get_x() < 362)
+	//{
+	//	cout << "Action!\n";
+	//}
+	
+	if (ref._v_x>obj._v_x)
+		if (ref.get_x() + ref.dstRect.w > obj.get_x())  // ref right bound is more than obj left bound
+			if (ref.get_x_() + ref.dstRect.w < obj.get_x())  // ref right bound is previously less than obj left bound 
+				if (ref.get_y() + ref.dstRect.h > obj.get_y())  // ref lower bound is lower than obj upper bound
+					if (ref.get_y() < obj.get_y() + obj.dstRect.h) // ref upper bound is higher than obj lower bound
 						res.set(Collision::Right);
 
 	if (ref._v_x<obj._v_x)
-		if (ref.dstRect.x < obj.dstRect.x + obj.dstRect.w)  // ref left bound is less than obj right bound
-			if (ref.dstRect_.x > obj.dstRect.x + obj.dstRect.w)  // ref left bound is previously more than obj right bound 
-				if (ref.dstRect.y + ref.dstRect.h > obj.dstRect.y)  // ref lower bound is lower than obj upper bound
-					if (ref.dstRect.y < obj.dstRect.y + obj.dstRect.h) // ref upper bound is higher than obj lower bound
+		if (ref.get_x() < obj.get_x() + obj.dstRect.w)  // ref left bound is less than obj right bound
+			if (ref.get_x_() > obj.get_x() + obj.dstRect.w)  // ref left bound is previously more than obj right bound 
+				if (ref.get_y() + ref.dstRect.h > obj.get_y())  // ref lower bound is lower than obj upper bound
+					if (ref.get_y() < obj.get_y() + obj.dstRect.h) // ref upper bound is higher than obj lower bound
 						res.set(Collision::Left);
 
 	if (ref._v_y>obj._v_y)
-		if (ref.dstRect.y + ref.dstRect.h > obj.dstRect.y) // ref lower bound is lower than obj upper bound
-			if (ref.dstRect_.y + ref.dstRect_.h < obj.dstRect.y) // ref lower bound is previously higher than obj upper bound 
-				if (ref.dstRect.x + ref.dstRect.w > obj.dstRect.x) // ref right bound is more than obj left bound
-					if (ref.dstRect.x < obj.dstRect.x + obj.dstRect.w) // ref left bound is less than obj right bound
+		if (ref.get_y() + ref.dstRect.h > obj.get_y()) // ref lower bound is lower than obj upper bound
+			if (ref.get_y_() + ref.dstRect.h < obj.get_y()) // ref lower bound is previously higher than obj upper bound 
+				if (ref.get_x() + ref.dstRect.w > obj.get_x()) // ref right bound is more than obj left bound
+					if (ref.get_x() < obj.get_x() + obj.dstRect.w) // ref left bound is less than obj right bound
 						res.set(Collision::Down);
 
 	if (ref._v_y<obj._v_y)
-		if (ref.dstRect.y < obj.dstRect.y + obj.dstRect.h)  // ref upper bound is higher than obj lower bound
-			if (ref.dstRect_.y > obj.dstRect.y + obj.dstRect.h)  // ref upper bound is previously lower than obj lower bound 
-				if (ref.dstRect.x + ref.dstRect.w > obj.dstRect.x)  // ref right bound is more than obj left bound
-					if (ref.dstRect.x < obj.dstRect.x + obj.dstRect.w) // ref left bound is higher than obj right bound
+		if (ref.get_y() < obj.get_y() + obj.dstRect.h)  // ref upper bound is higher than obj lower bound
+			if (ref.get_y_() > obj.get_y() + obj.dstRect.h)  // ref upper bound is previously lower than obj lower bound 
+				if (ref.get_x() + ref.dstRect.w > obj.get_x())  // ref right bound is more than obj left bound
+					if (ref.get_x() < obj.get_x() + obj.dstRect.w) // ref left bound is higher than obj right bound
 						res.set(Collision::Up);
+
+
+
 
 	if (res.count() > 0) res.set(Collision::Collided);
 	return res;
@@ -844,16 +905,15 @@ int main(int argc, char* argv[])
 	});
 
 	// Intro
-
-	Introscreen intro("Intro screen", displayManager.getScreenRenderer(), "../sprites/intro.png");
-	cout << "Intro start\n";
-	for (int n = 0; n < 6; ++n)
-	{
-		intro.draw();
-		intro.update();
-		SDL_RenderPresent(displayManager.getScreenRenderer());
-		std::this_thread::sleep_for(chrono::milliseconds(150));
-	}
+	//Introscreen intro("Intro screen", displayManager.getScreenRenderer(), "../sprites/intro.png");
+	//cout << "Intro start\n";
+	//for (int n = 0; n < 6; ++n)
+	//{
+	//	intro.draw();
+	//	intro.update();
+	//	SDL_RenderPresent(displayManager.getScreenRenderer());
+	//	std::this_thread::sleep_for(chrono::milliseconds(150));
+	//}
 	std::this_thread::sleep_for(chrono::milliseconds(2000));
 	cout << "Intro end\n";
 
@@ -869,35 +929,69 @@ int main(int argc, char* argv[])
 		switch (gameState)
 		{
 		case GameState::Menu:
+			cout << "Game Menu\n";
+			gameState = GameState::Playing;
 			break;
 		case GameState::Playing:
+			cout << "Game Playing\n";
+			for (auto &b : balls)
+			{
+				handleCollision(b, paddle);
+				handleCollision(b, aipaddle);
+				b.update();
+			}
+			// has the ball been lost?
+			{
+				
+				auto new_end = std::remove_if(balls.begin(), balls.end(), std::bind(&Ball::isOutOfScreen, std::placeholders::_1));
+				balls.erase(new_end, balls.end()); // still need to set the balls.end
+			}
+			//for (auto it = balls.begin(); 
+			//	it != balls.end(); )
+			//{
+			//	if (it->isOutOfScreen()) 
+			//	{ 
+			//		balls.erase(it); 
+			//		it = balls.begin(); // don't like this line
+			//	}
+			//	else 
+			//	{ 
+			//		++it; 
+			//	}
+			//}
+			//balls.erase(balls.begin());
+			//{
+			//	auto i = std::begin(balls);
+			//	while (i != std::end(balls))
+			//	{
+			//		// Do some stuff
+			//		if (i->isOutOfScreen())
+			//			i = balls.erase(i);
+			//		else
+			//			++i;
+			//	}
+			//}
+			paddle.update();
+			aipaddle.update(balls);
+			bg_parallax.update(paddle);
+
+			if(balls.size()==0)
+				gameState = GameState::GameOver;
 			break;
 		case GameState::GameOver:
+			cout << "Game Over\n";
 			break;
 		case GameState::HighScore:
+			cout << "High Score\n";
 			break;
 		case GameState::Exit:
+			cout << "Exit\n";
 			break;
 		default:
 			break;
 		}
 
-		for (auto &b : balls)
-		{
-			handleCollision(b, paddle);
-			handleCollision(b, aipaddle);
-			b.update();			
-		}
-		paddle.update();
-		aipaddle.update(balls);
-		bg_parallax.update(paddle);
 
-
-		//ball.update();
-		//paddle.update();
-		//aipaddle.update(ball);
-		//handleCollision(ball, paddle);
-		//handleCollision(ball, aipaddle);
 		t2 = chrono::high_resolution_clock::now();
 		this_thread::sleep_for(chrono::milliseconds(1000 / pollrate) - (t2 - t1));
 		t1 = t2;
